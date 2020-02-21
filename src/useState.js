c54834b0ebd8323c651e48f169a6c8329c594e9a
moss <dxp157@sohu.com>
2020-02-21 21:10:50 +0800

    解决多次 useState 时 state 之间冲突的问题

--- src/useState.js
+++ src/useState.js
@@ -1,20 +1,28 @@
 import ReactDom from 'react-dom'
 
 let _state // 全局变量
+let index = 0 // 解决 state 冲突的问题,因为有可能使用多个 useState
 
 const useState = initValue => {
+  // 保存当前的 index
+  let currentIndex = index
   // _state 用来保存一个全局的状态
-  _state = _state === undefined ? initValue : _state
+  _state[currentIndex] = _state[currentIndex] === undefined 
+    ? initValue 
+    : _state[currentIndex]
   // setState 是一个 callback, 只要 setState 被调用，就会重新 render 一遍
   // 同时 _state 也会得到更新
   const setState = newValue => {
-    _state = newValue
+    _state[currentIndex] = newValue
     render()
   }
-  return [_state, setState]
+  index += 1
+  return [_state[currentIndex], setState]
 }
 
 const render = () => {
+  // 每次渲染的时候应该把 index 清零
+  index = 0
   ReactDom.render(<App />, document.getElementById('root'))
 }
 
